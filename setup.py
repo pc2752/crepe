511c12387595599f0069e97bda7133ad6f7a71d7
pc2752 <pc2752@gmail.com>
2020-12-23 15:45:05 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -39,42 +39,42 @@ version = imp.load_source('crepe.version', os.path.join('crepe', 'version.py'))
 with open('README.md') as file:
     long_description = file.read()
 
-setup(
-    name='crepe',
-    version=version.version,
-    description='CREPE pitch tracker',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/marl/crepe',
-    author='Jong Wook Kim and Justin Salamon',
-    author_email='jongwook@nyu.edu',
-    packages=find_packages(),
-    entry_points = {
-        'console_scripts': ['crepe=crepe.cli:main'],
-    },
-    license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Multimedia :: Sound/Audio :: Analysis',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    keywords='tfrecord',
-    project_urls={
-        'Source': 'https://github.com/marl/crepe',
-        'Tracker': 'https://github.com/marl/crepe/issues'
-    },
-    install_requires=[
-        str(requirement)
-        for requirement in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
-    package_data={
-        'crepe': weight_files
-    },
-)
+# setup(
+#     name='crepe',
+#     version=version.version,
+#     description='CREPE pitch tracker',
+#     long_description=long_description,
+#     long_description_content_type='text/markdown',
+#     url='https://github.com/marl/crepe',
+#     author='Jong Wook Kim and Justin Salamon',
+#     author_email='jongwook@nyu.edu',
+#     packages=find_packages(),
+#     entry_points = {
+#         'console_scripts': ['crepe=crepe.cli:main'],
+#     },
+#     license='MIT',
+#     classifiers=[
+#         'Development Status :: 3 - Alpha',
+#         'License :: OSI Approved :: MIT License',
+#         'Topic :: Multimedia :: Sound/Audio :: Analysis',
+#         'Programming Language :: Python :: 2',
+#         'Programming Language :: Python :: 2.7',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.5',
+#         'Programming Language :: Python :: 3.6',
+#     ],
+#     keywords='tfrecord',
+#     project_urls={
+#         'Source': 'https://github.com/marl/crepe',
+#         'Tracker': 'https://github.com/marl/crepe/issues'
+#     },
+#     install_requires=[
+#         str(requirement)
+#         for requirement in pkg_resources.parse_requirements(
+#             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+#         )
+#     ],
+#     package_data={
+#         'crepe': weight_files
+#     },
+# )
